3e6c8ebc9e94499b1871ef50af1a47a6aef1567d
Wouter Wijsman <wwijsman@live.nl>
2017-10-23 13:05:47 +0200

    Removed math.lua It was no longer used

--- math.lua
+++
@@ -1,7 +0,0 @@
---Calculated the direction to a point in degrees
-function directionToPoint(orgX, orgY, aimX, aimY)
-  deltaX = aimX - orgX
-  deltaY = aimY - orgY
-  result = math.atan2(deltaX, deltaY)
-  return math.deg(result)
-end
